55da06db23a12783ee8630d299532dc0117a2159
Satish Yadav <satish.yadav@nagarro.com>
2021-07-28 17:18:00 +0530

    Type Inference Examples

---
+++ ts/typeInference.ts
@@ -0,0 +1,27 @@
+//Premitives
+let bananas = 5;
+let accleration = 'fast';
+let isItFine = true;
+
+let nothingInThere = null;
+let voidSpace = undefined;
+
+//built in Objects
+let currentDate = new Date();
+
+//Array
+const colours = ['red', 'green', 'blue', 'pink'];
+const randomNum = [1, 23, 3.2, 0.0];
+const isItAllFine = [true, false, true];
+
+//Custom class
+class Max {}
+const max = new Max();
+
+//Object literal
+const plot = { x: 2, y: 3 };
+
+//Function
+const logName = (name: string): void => {
+	console.log(name);
+};
