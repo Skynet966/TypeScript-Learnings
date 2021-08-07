bcddcfe622804ef77c52a1e7561ff670a12d3bb5
Satish Yadav <satish.yadav@nagarro.com>
2021-08-07 17:30:25 +0530

    sorting a number of array

--- build/sort.js
+++ build/sort.js
@@ -1,3 +1,23 @@
 "use strict";
-console.log('quick brown fox jumps over the lazy dog.');
+var Sorter = (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+sorter.sort();
+console.log(sorter.collection);
 //# sourceMappingURL=sort.js.map
--- src/sort.ts
+++ src/sort.ts
@@ -1,1 +1,20 @@
-console.log('quick brown fox jumps over the lazy dog.');
+//Sorting logic for the array of numbers
+class Sorter {
+	constructor(public collection: number[]) {}
+	sort(): void {
+		const { length } = this.collection;
+		for (let i = 0; i < length; i++) {
+			for (let j = 0; j < length - i - 1; j++) {
+				if (this.collection[j] > this.collection[j + 1]) {
+					let leftHand = this.collection[j];
+					this.collection[j] = this.collection[j + 1];
+					this.collection[j + 1] = leftHand;
+				}
+			}
+		}
+	}
+}
+
+const sorter = new Sorter([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+sorter.sort();
+console.log(sorter.collection);
