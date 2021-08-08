63d55c86638a1a753e9da891c9641655e66b5308
Satish Yadav <satish.yadav@nagarro.com>
2021-08-08 09:23:40 +0530

    Basic example of TS

--- src/basic.ts
+++ src/basic.ts
@@ -1,5 +1,4 @@
 //Basic example of Typescript in use.
-
 import axios from 'axios';
 
 interface Todo {
@@ -13,14 +12,19 @@ interface Todo {
 const url: string = 'https://jsonplaceholder.typicode.com/todos/1';
 axios
 	.get(url)
-	.then(response => {
+	.then((response) => {
 		const { id, title, completed } = response.data as Todo;
 		logTodo(id, title, completed);
 	})
-	.catch(errors => console.log(errors));
+	.catch((errors) => console.log(errors));
 
-const logTodo = (id: number, title: string, completed: boolean) =>
-	console.log(id, title, completed);
-//Change the text on the page
-// let webHeading = document.querySelector('h1');
-// webHeading!.textContent = 'h';
+let webId = document.querySelector('h2');
+let webTitle = document.querySelector('h3');
+let webStatus = document.querySelector('h4');
+const logTodo = (id: number, title: string, completed: boolean) => {
+	webId!.textContent = `Id: ${id}`;
+	webTitle!.textContent = `Title: ${title}`;
+	webStatus!.textContent = `Is Completed: ${completed}`;
+};
+
+//Use "parcel index.html" to run the program
