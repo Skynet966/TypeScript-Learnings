a2d55b7953eb7d0019c0509b2ab1351eafba560c
Satish Yadav <satish.yadav@nagarro.com>
2021-08-13 14:24:21 +0530

    Refactor the code to support multiple readers

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,10 +1,7 @@
-import { MatchResult } from './MatchResult';
-import { dateStringToDate } from './utils';
 import { readFileSync } from 'fs';
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-export default class CsvFileReader {
-	data: MatchData[] = [];
+export default abstract class CsvFileReader<T> {
+	data: T[] = [];
 	constructor(public filename: string) {}
 	read(): void {
 		this.data = readFileSync(this.filename, { encoding: 'utf-8' })
@@ -12,13 +9,6 @@ export default class CsvFileReader {
 			.map((row: string): string[] => row.split(','))
 			.map(this.mapRow);
 	}
-	mapRow = (row: string[]): MatchData => [
-		dateStringToDate(row[0]),
-		row[1],
-		row[2],
-		parseInt(row[3]),
-		parseInt(row[4]),
-		row[5] as MatchResult,
-		row[6]
-	];
+
+	abstract mapRow(row: string[]): T;
 }
