5a9eb296e5f5788aa3a55b569b88c371fa700327
Satish Yadav <satish.yadav@nagarro.com>
2021-08-26 15:04:30 +0530

    remove unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import MatchReader from './MatchReader';
-import CsvFileReader from './CsvFileReader';
-import ConsoleReport from './reportTargets/ConsoleReport';
-import WinsAnalysis from './analyzers/WinsAnalysis';
 import Summary from './Summary';
-import HtmlReport from './reportTargets/HtmlReport';
 
 //Static way to generate Summary instance.
 const matchReader = MatchReader.readFromCsv('football');
